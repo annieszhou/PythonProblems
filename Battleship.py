f2af4ccf4079f6877676505375f991d15590d6b0
annieszhou <annieszhou24@gmail.com>
2022-08-10 19:54:24 +0000

    battleship

--- Battleship.py
+++ Battleship.py
@@ -1,10 +1,39 @@
-def place_ship():
+
+
+
+def place_ship(array, integer, character, board):
     print("placing ship ...")
 
+    if "R" is character:
+        board[i//5][i%5]="S"
+        board[i//5][i%5-1]="S"
+        board[i//5][i%5-2]="S"
+
+    if "L" is character:
+        board[i//5][i%5]="S" 
+        board[i//5][i%5+1]="S" 
+        board[i//5][i%5+2]="S" 
+
+    if "U" is character:
+        board[i//5][i%5]="S" 
+        board[i//5-1][i%5]="S" 
+        board[i//5-2][i%5]="S" 
+
+    if "D" is character:
+        board [i//5][i%5]="S" 
+        board[i//5+1][i%5]="S" 
+        board[i//5+2][i%5]="S"
+
+
+
+    
+    
 
 
 if __name__ == '__main__':
     print("Battleship")
+
+
 
     array_1 = [["S", " ", " ", " ", " "],
                ["S", " ", " ", " ", " "],
@@ -19,3 +48,5 @@ if __name__ == '__main__':
                [" ", " ", " ", " ", " "]]
 
     place_ship(array_1, 15, "R")
+
+    print("array_1")
